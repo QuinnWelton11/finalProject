db44ade66f63ba654278f66978537dc5cad2edfd
Quinn Welton <quinn.welton@gmail.com>
2025-04-15 16:57:00 -0700

    Added difficulty levels, game logic, and styling changed for mobile(made content smaller to fit better on mobile screens)

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,43 @@
 const cells = document.querySelectorAll(".cell");
+
 const statusText = document.querySelector(".statusText");
+
+const difficultyLevel = document.getElementById("difficultyLevel");
+
+let difficulty = "normal";
+
+const difficultyDesc = document.getElementById("difficultyDesc");
+
+const difficultyDescriptions = {
+
+    easy: "I don't want a challenge...",
+
+    normal: "I like a good challenge!",
+
+    hard: "I want a worthy opponent!",
+
+    legendary: "The bots will fear me!"
+
+};
+
+difficultyLevel.addEventListener("change", () => {
+
+    difficulty = difficultyLevel.value;
+
+    difficultyDesc.textContent = difficultyDescriptions[difficulty];
+
+});
+
 const restartBtn = document.querySelector("#restartBtn");
 
 const winConditions = [
-
+    
     //Row win begin
-
+    
     [0, 1, 2],
-
+    
     [3, 4, 5],
-
+    
     [6, 7, 8],
 
     //Row win end
@@ -56,6 +84,120 @@ function startGame() {
 }
 
 function computerMove() {
+
+    let randomChance;
+
+    if (difficulty === "easy") {
+
+        randomChance = 0.45;
+
+    } else if (difficulty === "normal") {
+
+        randomChance = 0.6;
+
+    } else if (difficulty === "hard") {
+
+        randomChance = 0.7;
+
+    } else {
+
+        randomChance = 1.0;
+
+    }
+
+    const randomRoll = Math.random();
+
+    console.log(`Difficulty: ${difficulty}, Random chance: ${randomChance}, Rolled: ${randomRoll}`);
+
+
+    if (randomRoll < randomChance) {
+    
+        for (let condition of winConditions) {
+
+            const [a, b, c] = condition;
+
+            const gridLine = [options[a], options[b], options[c]];
+
+            if (gridLine.filter(val => val === "O").length == 2 && gridLine.includes("")) {
+
+                const emptyCell = condition[gridLine.indexOf("")];
+
+                updateCell(cells[emptyCell], emptyCell);
+
+                checkWinner();
+
+                return;
+
+            }
+
+        }
+
+    
+
+        for (let condition of winConditions) {
+
+            const [a, b, c] = condition;
+
+            const gridLine = [options[a], options[b], options[c]]; 
+
+            if (gridLine.filter(val => val === "X").length == 2 && gridLine.includes("")) {
+
+                const emptyCell = condition[gridLine.indexOf("")];
+
+                updateCell(cells[emptyCell], emptyCell);
+
+                checkWinner();
+
+                return;
+
+            }
+
+        }    
+
+        if (options[4] === "") {
+
+            updateCell(cells[4], 4);
+
+            checkWinner();
+
+            return;
+
+        }
+
+        const player = "X";
+
+        if ((options[0] === player && options[8] === player && options[4] === "O") || (options[2] === player && options[6] === player && options[4] === "O")) {
+
+            const gridEdges = [1, 3, 5, 7];
+
+            const gridEdge = gridEdges.find(index => options[index] === "");
+
+            if (gridEdge !== undefined) {
+
+                updateCell(cells[gridEdge], gridEdge);
+
+                checkWinner();
+
+                return;
+
+            }
+
+        }
+
+        const gridCorners = [0, 2, 6, 8];
+
+        const emptyCorner = gridCorners.find(index => options[index] === "");
+
+        if (emptyCorner !== undefined) {
+
+            updateCell(cells[emptyCorner], emptyCorner);
+
+            checkWinner();
+
+            return;
+
+        }
+    }
 
     let emptyCells = [];
 
@@ -157,7 +299,7 @@ function checkWinner () {
 
         statusText.textContent = `It's a Draw!`;
 
-        running = false;
+        playing = false;
 
     } else {
 
@@ -183,22 +325,29 @@ function restartGame() {
 
 const music = document.getElementById("background_music");
 
-    music.volume = 0.2;
+const video = document.getElementById("backgroundVideo");
 
-const toggleBtn = document.getElementById("musicToggle")
+const mediaToggle = document.getElementById("mediaToggle");
 
-toggleBtn.addEventListener("click", () => {
+music.volume = 0.2;
 
-    if (music.muted) {
+mediaToggle.addEventListener("click", () => {
 
-        music.muted = false;
+    if (music.paused) {
 
-        toggleBtn.textContent = "🔇";
+        music.play()
+
+        video.play()
+
+        mediaToggle.innerText = "⏸️";
 
     } else {
 
-        music.muted = true;
-        toggleBtn.textContent = "🔊";
+        music.pause()
+
+        video.pause()
+
+        mediaToggle.innerText = "▶️";
 
     }
 
